8d5788905db981582f2af57e1a7b3bc5d50b4d69
Stijn Beauprez <stijnbe@gmail.com>
2022-03-16 11:50:19 +0100

    remove unused functions

--- components/map/MainMap.tsx
+++ components/map/MainMap.tsx
@@ -4,9 +4,7 @@ import mapboxgl from "mapbox-gl"; // eslint-disable-line import/no-webpack-loade
 export default function MainMap() {
   const mapContainer = useRef(null);
   const map = useRef<mapboxgl.Map | undefined>(undefined);
-  const [lng, setLng] = useState(2.748);
-  const [lat, setLat] = useState(51.1284);
-  const [zoom, setZoom] = useState(9);
+  const [zoom] = useState(9);
 
   useEffect(() => {
     if (map.current) return; // initialize map only once
@@ -15,7 +13,7 @@ export default function MainMap() {
     const mapboxMap = new mapboxgl.Map({
       container: mapContainer.current,
       style: "mapbox://styles/mapbox/streets-v11",
-      center: [lng, lat],
+      center: [2.748, 51.1284],
       zoom: zoom,
     });
     map.current = mapboxMap;
